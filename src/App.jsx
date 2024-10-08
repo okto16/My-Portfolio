22f376a2caea7bd93f7c0d7ef013a5c57d89d571
okto2000 <oktorino2000@gmail.com>
2024-10-08 16:33:25 +0700

    V2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./Layouts/Layout";
-import "./Index.css";
+import "./index.css";
 import Project from "./component/Project";
 // import Card from "./component/Card";
 import Home from "./component/Home";
