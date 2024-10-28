e2245dab7c13ce1a3e2d347fe03102e03d10f9ac
okto16 <bagastx@gmail.com>
2024-10-28 16:28:00 +0700

    V2

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,32 +3,15 @@ import Github from "../assets/git.png";
 import Instagram from "../assets/instagram.png";
 const Footer = () => {
   const socialLinks = [
-    {
-      href: "https://github.com/okto16",
-      icon: Github,
-      size: "w-4 h-4 sm:w-6 sm:h-6 lg:w-8 lg:h-8",
-      name: "GitHub",
-    },
-    {
-      href: "https://www.instagram.com/oktorino_bagas/",
-      icon: Instagram,
-      size: "w-5 h-5 sm:w-7 sm:h-7 lg:w-8 lg:h-8",
-      name: "Instagram",
-    },
-    {
-      href: "https://www.linkedin.com/in/oktorino-bagas-aji-sudarno",
-      icon: Linkedin,
-      size: "w-4 h-4 sm:w-6 sm:h-6 lg:w-8 lg:h-8",
-      name: "Linkedin",
-    },
+    { href: "https://github.com/okto16", icon: Github, size: "w-4 h-4 sm:w-6 sm:h-6 lg:w-8 lg:h-8",name: "GitHub" },
+    { href: "https://www.instagram.com/oktorino_bagas/", icon: Instagram, size: "w-5 h-5 sm:w-7 sm:h-7 lg:w-8 lg:h-8",name: "Instagram" },
+    { href: "https://www.linkedin.com/in/oktorino-bagas-aji-sudarno", icon: Linkedin, size: "w-4 h-4 sm:w-6 sm:h-6 lg:w-8 lg:h-8",name: "Linkedin" },
   ];
 
   return (
     <footer className="py-8 sm:py-12 lg:py-16">
       <div className="text-center">
-        <h2 className="text-lg sm:text-2xl lg:text-3xl mb-4 sm:mb-6" style="">
-          Connect with me
-        </h2>
+        <h2 className="text-lg sm:text-2xl lg:text-3xl mb-4 sm:mb-6">Connect with me</h2>
         <div className="flex justify-center items-center space-x-4 sm:space-x-6">
           {socialLinks.map((link, index) => (
             <a
