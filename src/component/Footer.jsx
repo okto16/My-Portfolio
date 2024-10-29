fb530b834a997343b67cb7a02e66e8316da32492
okto16 <bagastx@gmail.com>
2024-10-29 10:24:05 +0700

    Update Footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,9 +3,9 @@ import Github from "../assets/github-seeklogo.svg";
 import Instagram from "../assets/instagram.png";
 const Footer = () => {
   const socialLinks = [
-    { href: "https://github.com/okto16", icon: Github, size: "w-4 h-4 sm:w-6 sm:h-6 lg:w-8 lg:h-8",name: "GitHub" },
-    { href: "https://www.instagram.com/oktorino_bagas/", icon: Instagram, size: "w-5 h-5 sm:w-7 sm:h-7 lg:w-8 lg:h-8",name: "Instagram" },
-    { href: "https://www.linkedin.com/in/oktorino-bagas-aji-sudarno", icon: Linkedin, size: "w-4 h-4 sm:w-6 sm:h-6 lg:w-8 lg:h-8",name: "Linkedin" },
+    { href: "https://github.com/okto16", icon: Github,name: "GitHub" },
+    { href: "https://www.instagram.com/oktorino_bagas/", icon: Instagram,name: "Instagram" },
+    { href: "https://www.linkedin.com/in/oktorino-bagas-aji-sudarno", icon: Linkedin,name: "Linkedin" },
   ];
 
   return (
@@ -21,7 +21,7 @@ const Footer = () => {
               rel="noopener noreferrer"
               className="transition-transform hover:scale-110 text-sm flex flex-col items-center"
             >
-              <img src={link.icon} alt="" className={link.size} />
+              <img src={link.icon} alt="" className="w-5 h-5 sm:w-7 sm:h-7 lg:w-8 lg:h-8" />
               {link.name}
             </a>
           ))}
