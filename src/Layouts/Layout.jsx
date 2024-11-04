6928c8f94928ce3691832cc1c450f9afc41c70d1
okto16 <bagastx@gmail.com>
2024-11-04 16:56:13 +0700

    Add Analytics

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -10,6 +10,7 @@ import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { Stars } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 const COLORS = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C"];
 const Layout = () => {
@@ -28,6 +29,7 @@ const Layout = () => {
       className="relative min-h-screen w-full overflow-hidden bg-gray-950 text-gray-200"
       style={{ backgroundImage }}
     >
+      <Analytics />
       <div className="relative z-10 mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8">
         <header>
           <Navbar />
